b68e82e7b5d7564d37a53ab65423fddcdba1ae58
Ado <adnandzebic@gmail.com>
2014-10-26 01:48:05 -0700

    boomshakalaka!

--- javascripts/secondplot.js
+++ javascripts/secondplot.js
@@ -1,8 +1,8 @@
 $(function () {
     var d1 = [];
-    for (var i = 0; i < 14; i += 0.1)
+    for (var j = 0; j < 14; j += 0.1)
     {
-        d1.push([i, Math.cos(i)]);
+        d1.push([i, math.sqrt(i)]);
     }
 
 $.plot("#placeholder2", [
