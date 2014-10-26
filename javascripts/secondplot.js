474b60218895148b07b283b51cab10d9ac2f54fb
Ado <adnandzebic@gmail.com>
2014-10-26 03:22:58 -0700

    boomshakalaka!

--- javascripts/secondplot.js
+++ javascripts/secondplot.js
@@ -5,7 +5,7 @@ $(function () {
     {
       //var expression = sprintf('%d * %d',j,j);
       var scope = {s:s};
-      var hs = '(2*s*i + 200)/(s*i*(s*i+10))';
+      var hs = 's*i*(s*i+50)/((s*i)^2+s*i+16)';
         d1.push([s, math.eval('20*log(sqrt(re(' + hs + ')^2 + im(' + hs + ')^2))' , scope)]);
     }
 
