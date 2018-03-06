4e648160911162a7fe93db4c3df675a9773f2530
Adnan <Adnan.Dzebic@idt.com>
2018-03-06 13:09:03 -0800

    test

--- resources/js/dongle.js
+++ resources/js/dongle.js
@@ -34,6 +34,27 @@ function print() {
 
 }
 
+function write(device) {
+  var data1 = new Uint8Array(8);
+  data1[0] = 0x50;
+  data1[1] = 0x03;
+  data1[2] = 0x00;
+  //var data1 = [0x50, 0x03, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00];
+  device.transferOut(1, data1);
+
+  data1[0] = 0x45;
+  data1[1] = 0x40;
+  data1[2] = 0x3b;
+  data1[3] = 0x0f;
+  data1[6] = 0x01;
+
+  device.transferOut(1, data1);
+  //device.close();
+  //var data2 = [0x45, 0x40, 0x3b, 0x0f, 0x00, 0x00, 0x01, 0x00];
+  //device.transferOut(1, data2);
+
+}
+
 function connect() {
   if (device == null) {
     navigator.usb.requestDevice({ filters: [{ vendorId: 0x0426 }] })
